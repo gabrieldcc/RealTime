7e06a6d072c653d196cf07adfbbf7edb63c71e07
Gabriel Castro <gabrieldcc@gmail.com>
2023-08-13 10:04:42 -0300

    Initial Commit

---
+++ RealTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTime
+//
+//  Created by Gabriel de Castro Chaves on 13/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
